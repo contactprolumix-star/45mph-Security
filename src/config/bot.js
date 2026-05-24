6e860ac67ed28dfd9c122205257d6de6fcceada7
contactprolumix-star <contactpro.lumix@gmail.com>
2026-05-24 11:26:06 +0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "/help | 45mph Security",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 1, 
       },
     ],
   },
@@ -88,14 +88,14 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
+      primary: "A47DAB", 
       secondary: "#2F3136", 
 
       // Standard status colors for success/error/warning/info messages.
-      success: "#57F287", 
-      error: "#ED4245", 
-      warning: "#FEE75C", 
-      info: "#3498DB", 
+      success: "A47DAB", 
+      error: "A47DAB", 
+      warning: "A47DAB", 
+      info: "A47DAB", 
 
       // Neutral utility colors.
       light: "#FFFFFF",
